be95442e04725ebaf1c109a00293d015376767ab
Nöelle <63829617+noelledotjpg@users.noreply.github.com>
2025-09-13 20:50:37 -0300

    refactor .css to .scss
    - removed theme toggling

--- theme.js
+++ theme.js
@@ -16,36 +16,4 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   }
-
-  // Tema claro/escuro
-  const themeToggle = document.getElementById("theme-toggle");
-  const iconSun = document.getElementById("icon-sun");
-  const iconMoon = document.getElementById("icon-moon");
-  const themeText = document.getElementById("theme-text");
-
-  function updateThemeUI(isDark) {
-    if (isDark) {
-      iconSun.classList.add("hidden");
-      iconMoon.classList.remove("hidden");
-      themeText.textContent = "Tema: Escuro";
-    } else {
-      iconSun.classList.remove("hidden");
-      iconMoon.classList.add("hidden");
-      themeText.textContent = "Tema: Claro";
-    }
-  }
-
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme === "dark") {
-    document.documentElement.classList.add("dark");
-    updateThemeUI(true);
-  } else {
-    updateThemeUI(false);
-  }
-
-  themeToggle.addEventListener("click", () => {
-    const isDark = document.documentElement.classList.toggle("dark");
-    updateThemeUI(isDark);
-    localStorage.setItem("theme", isDark ? "dark" : "light");
-  });
 });
